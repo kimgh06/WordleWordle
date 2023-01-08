b7543bf60e3f3a52828dea02a6728ec6e07c90a3
kkh061101 <kkh061101@naver.com>
2023-01-08 14:45:20 +0900

    입력 받게 만듦

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './main.scss';
+import fs from "fs";
 
 function Main() {
+  useEffect(() => {
+    fs.readFile('https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt', 'utf-8',
+      (e, data) => e ? console.error(e) : console.log(data));
+  }, []);
   return <div className="main">
-    hello wordle!
+    <Letter value={window.onkeyup = e => {
+      if (e.key === 'Backspace' || e.key === 'Enter' || ('z' >= e.key && e.key >= 'a')) {
+        console.log(e.key);
+        return e.key;
+      }
+    }} filled={false} />
+  </div>
+}
+
+function Letter(props) {
+  const [filled, setFilled] = useState(false);
+  return <div className={"letter" + filled ? "filled" : ''}>
+    {props.value}
   </div>
 }
 
